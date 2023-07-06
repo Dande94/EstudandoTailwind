7d9dce296ec72a6ad206cb814dff03d866918c01
Anderson Nunes <dande.nunes.94@gmail.com>
2023-07-06 18:45:16 -0300

    Modulo 3 - customizando com cores

--- instalandoTW/tailwind.config.js
+++ instalandoTW/tailwind.config.js
@@ -4,11 +4,25 @@ module.exports = {
     "./src/**/*.{html,ts,js,php}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'vermelho' : '#FF0000',
+        'vermelhoEscuro' : '#990000'
+      }
+    },
   },
   plugins: [],
 }
 
 /*em content[], é setado um objeto que diz quais arquivos ele deve observar:
 na linha 4 está dizendo-> volte um direttório/ acesse o diretório 'src' / entre em todos os diretório de 'src' /  aplique tailwind em todos os arquivos com as seguintes extenções .{html,ts,js,php};
+*/
+/*
+extend: {
+      colors:{
+        'vermelho' : '#FF0000',
+        'vermelhoEscuro' : '#990000'
+      }
+    }
+--extendo cores para serem usadas no TW;
 */
