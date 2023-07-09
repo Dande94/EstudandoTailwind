40fedf9f778df6a4b18c303b65975c8002f9ad27
Anderson Nunes <dande.nunes.94@gmail.com>
2023-07-09 16:36:12 -0300

    Modulo 5 - Definindo um prefixo

--- acoesComInteracoes/tailwind.config.js
+++ acoesComInteracoes/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // prefix: 'b7-',//aplica um prefixo para usar as classes tailwind
   content: [
     "./src/**/*.{html,ts,js,php}"
   ],
